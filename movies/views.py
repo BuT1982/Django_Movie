a9aad4eb6dcbf38d9bcc147101a5955ca40f48c3
BuT1982 <but1982@mail.ru>
2021-01-13 21:44:34 +0300

    add templates movies.html movie_detail.html

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from movies.models import Movie
 
-# Create your views here.
+
+# GenreYear
+# MoviesView
+# MovieDetailView
+# AddReview
+# ActorView
+# FilterMoviesView
+# JsonFilterMoviesView
+# AddStarRating
+# Search
+
+
+class MoviesView(ListView):
+    """ Список фильмов """
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    context_object_name = 'movies'
+    template_name = 'movies/movies.html'
+
+
+class MovieDetailView(DetailView):
+    """ Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
